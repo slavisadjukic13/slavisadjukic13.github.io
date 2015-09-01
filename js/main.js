88f0e695312d13791ba24ad2a5f23991fcfb2922
slavisadjukic13 <slavisadjukic13@gmail.com>
2015-09-01 18:16:51 +0200

    main menu structure & style updated

--- js/main.js
+++ js/main.js
@@ -1,56 +1,30 @@
-// Show dropdown on hover - enable possibility link on trigger
-$.fn.oDropdownHover = function (dropdownMenuInterval) {
-    var $dropdownMenuInterval = dropdownMenuInterval || 50;
-
-    $(this).each(function () {
-        var $this = $(this),
-            $dropdownMenu = $this.find('.dropdown-menu'),
-            $dropdownAnchor = $this.find('.dropdown-toggle'),
-            $dropdownMenuTimeout;
-
-        // Make dropdown accessble on hover
-        $this.on('mouseover', function () {
-            clearTimeout($dropdownMenuTimeout);
-            $this
-                .addClass('open')
-                .find('.dropdown-toggle')
-                .attr('aria-expanded', 'true');
-        }).on('mouseleave', function () {
-            $dropdownMenuTimeout = setTimeout(function () {
-                $this
-                    .removeClass('open')
-                    .find('.dropdown-toggle')
-                    .attr('aria-expanded', 'false');
-            }, $dropdownMenuInterval);
-        });
-
-        // Make anchor clickable
-        $dropdownAnchor.on('click', function (e) {
-            e.stopPropagation();
-            if ($dropdownAnchor.attr('target') == '_blank') {
-                window.open(this.href);
-                return false;
-            } else {
-                window.location = this.href;
-            }
-        });
-
-        // Clear timeout to disable flickering
-        $dropdownMenu.on('mouseover', function () {
-            clearTimeout($dropdownMenuTimeout);
-        });
-    });
-
-} //-- End oNavbarDropdown
-
 $(document).ready(function () {
-
-    $('.navbar li.dropdown').oDropdownHover();
-
     // Menu toggle effect
     $('.navbar-toggle').on('click', function () {
         $(this).toggleClass('active');
+        $('.main-menu').toggleClass('active');
         $('.icon-bar').toggleClass('rotated');
     });
 
-})
+    $('.main-menu li a').on('click', function () {
+        $(this).toggleClass('active');
+        $('.main-menu').toggleClass('active');
+        $('.icon-bar').toggleClass('rotated');
+    });
+
+});
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});

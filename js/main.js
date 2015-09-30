267a4496499f4b6156cde319964f790e1d73f601
slavisadjukic13 <slavisadjukic13@gmail.com>
2015-09-30 12:50:39 +0200

    main index - js fixed (collapse and scrool working together)

--- js/main.js
+++ js/main.js
@@ -14,8 +14,24 @@ $(document).ready(function () {
 
 });
 
-$(function() {
+
+/*$(function() {
     $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});*/
+
+$(function() {
+    $('a[href*=#]:not([data-toggle=collapse])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');

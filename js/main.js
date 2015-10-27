2b014a14ffc5dbc0c0ab7c9f2a1c6a811a5220e0
slavisadjukic13 <slavisadjukic13@gmail.com>
2015-10-27 01:53:53 +0100

    new sideproject :D

--- js/main.js
+++ js/main.js
@@ -1,48 +1,5 @@
 $(document).ready(function () {
-    // Menu toggle effect
-    $('.navbar-toggle').on('click', function () {
-        $(this).toggleClass('active');
-        $('.main-menu').toggleClass('active');
-        $('.icon-bar').toggleClass('rotated');
-    });
-
-    $('.main-menu li a').on('click', function () {
-        $(this).toggleClass('active');
-        $('.main-menu').toggleClass('active');
-        $('.icon-bar').toggleClass('rotated');
-    });
-
+    $(".main").onepage_scroll();
 });
 
 
-/*$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
-});*/
-
-
-
-$(function() {
-    $('a[href*=#]:not([data-toggle=collapse])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
-            }
-        }
-    });
-});

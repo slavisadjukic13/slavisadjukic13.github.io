6236a0030ce0c5ca2b410f6e7261178b5f84c819
slavisadjukic13 <slavisadjukic13@users.noreply.github.com>
2016-02-07 02:29:10 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>php file</title>
+
+
+
+
+
+</head>
+<body>
+
+
+<p>proba ph file</p>
+
+
+<?php
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
+
+</body>
+
+</html>

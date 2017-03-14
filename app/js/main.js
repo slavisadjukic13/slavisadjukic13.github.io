c6a10a0df16a15c928973c2c2cd1d7cfa80e5986
slavisadjukic13 <slavisadjukic13@gmail.com>
2017-03-14 15:25:13 +0100

    domba - something :D

--- app/js/main.js
+++ app/js/main.js
@@ -122,6 +122,45 @@ $(document).ready(function() {
     //     postfix: " m&sup2"
     // });
 
+    var $eventLog = $(".js-event-log");
+    var $eventSelect = $(".hero #ad-type");
+
+    // $eventSelect.on("select2:open", function (e) { log("select2:open", e); });
+    // $eventSelect.on("select2:close", function (e) { log("select2:close", e); });
+    $eventSelect.on("select2:select", function (e) { log("select2:select", e); });
+    $eventSelect.on("select2:unselect", function (e) { log("select2:select", e); });
+    // $eventSelect.on("select2:unselect", function (e) { log("select2:unselect", e); });
+
+    $eventSelect.on("change", function (e) { log("change"); });
+
+    function log (name, evt) {
+        if (!evt) {
+            var args = "{}";
+        } else {
+            var args = JSON.stringify(evt.params, function (key, value) {
+                if (value && value.nodeName) return "[DOM node]";
+                if (value instanceof $.Event) return "[$.Event]";
+                return value;
+            });
+        }
+        var $e = $("<li>" + name + " -> " + args + "</li>");
+        // $eventLog.append($e);
+        console.log(args.data.text);
+        console.log(args);
+        // console.log(evt.params);
+        // console.log(args.data);
+        // $e.animate({ opacity: 1 }, 10000, 'linear', function () {
+        //     $e.animate({ opacity: 0 }, 2000, 'linear', function () {
+        //         $e.remove();
+        //     });
+        // });
+
+        // if (args.)
+    }
+
+    
+
+
 
 
     $('.footer-most-viewed-list').slick({
